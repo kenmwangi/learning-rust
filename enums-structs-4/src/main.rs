ce17115daee91b4fef7ed253a08302828a9d28a6
Ken Mwangi <kenmwang071@gmail.com>
2023-07-31 16:51:22 +0300

    Ownership

--- enums-structs-4/src/main.rs
+++ enums-structs-4/src/main.rs
@@ -1,26 +1,44 @@
 fn main() {
-    struct Employee {
-        name: String,
-        age: u8,
-        email_id: String,
-        experience: u8,
-    }
+    // Ownership
 
-    impl Employee {
-        // Employee fields acessed through "self" using dot operator
-        fn get_name(&self) -> &str {
-            &self.name
-        }
+    // literals are immutable
+    let _st = "hello";
 
-        fn set_age(&mut self, age: u8) {
-            self.age = age;
-        }
+    // String is
+    let s1 = String::from("ken");
+    let s2 = s1.clone();
 
-        fn print_employee(&self) {
-            println!(
-                "Name : {}, Age : {}, email_id = {}, experience = {}",
-                self.name, self.age, self.email_id, self.experience
-            )
-        }
-    }
+    println!("s1 = {}, s2 = {}", s1, s2);
+
+    // Stack-Only Data: copy
+
+    let x = 10;
+    let y = x;
+    println!("x = {}, y = {}", x, y);
+
+    // Ownership and Functions
+
+    let name = String::from("hello");
+    takes_ownership(name);
+
+    let num = 5;
+    makes_copy(num);
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}", s1, len);
+}
+
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope
+    println!("{}", some_string);
+} // drop is called. Backing memory is freed
+
+fn makes_copy(some_integer: i32) {
+    // some_integer comes into scope
+    println!("{}", some_integer);
+} // some_integer goes out of scope.Nothing special happens
+
+fn calculate_length(s: &String) -> usize {
+    s.len();
 }
