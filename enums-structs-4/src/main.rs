78df82c0239258224d2216c65d7e7f5512acf9b0
Ken Mwangi <kenmwang071@gmail.com>
2023-08-01 10:34:48 +0300

    structs

--- enums-structs-4/src/main.rs
+++ enums-structs-4/src/main.rs
@@ -1,44 +1,36 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+impl Rectangle {
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+}
 fn main() {
-    // Ownership
+    let rect1 = Rectangle {
+        width: 50,
+        height: 30,
+    };
 
-    // literals are immutable
-    let _st = "hello";
+    println!(
+        "The area of the rectangle is {} square pixels",
+        rect1.area()
+    );
 
-    // String is
-    let s1 = String::from("ken");
-    let s2 = s1.clone();
-
-    println!("s1 = {}, s2 = {}", s1, s2);
-
-    // Stack-Only Data: copy
-
-    let x = 10;
-    let y = x;
-    println!("x = {}, y = {}", x, y);
-
-    // Ownership and Functions
-
-    let name = String::from("hello");
-    takes_ownership(name);
-
-    let num = 5;
-    makes_copy(num);
-
-    let len = calculate_length(&s1);
-
-    println!("The length of '{}' is {}", s1, len);
+    if rect1.width() {
+        println!("The Rectangle has a nonzero width: it is {}", rect1.width);
+    }
 }
 
-fn takes_ownership(some_string: String) {
-    // some_string comes into scope
-    println!("{}", some_string);
-} // drop is called. Backing memory is freed
-
-fn makes_copy(some_integer: i32) {
-    // some_integer comes into scope
-    println!("{}", some_integer);
-} // some_integer goes out of scope.Nothing special happens
-
-fn calculate_length(s: &String) -> usize {
-    s.len();
-}
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.height * rectangle.width
+// }
